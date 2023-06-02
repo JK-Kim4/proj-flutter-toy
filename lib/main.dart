ca20dc79524061e19994929564fced98b78a0e44
jongwankim <jongbell4@gmail.com>
2023-06-02 17:57:48 +0900

    feat: flutter toy project

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,32 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 
-/**
- * At the very top of the file, you'll find the main() function.
- * In its current form, it only tells Flutter to run the app defined in MyApp.
- * */
-void main() {
-  runApp(MyApp());
-}
 
-/**
- * The MyApp class extends StatelessWidget.
- * Widgets are the elements from which you build every Flutter app.
- * As you can see, even the app itself is a widget.
- * */
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(MaterialApp(
+  home: JWCard(),
+));
+
+class JWCard extends StatelessWidget {
+  const JWCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext(){
-    current = WordPair.random();
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-              onPressed: (){
-                appState.getNext();
-              },
-              child: Text('Next'),
-          ),
-        ],
-      )
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    Key? key,
-    required this.pair,
-  }) : super(key: key);
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context){
-    var theme = Theme.of(context);
-    var style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: pair.asPascalCase,
-        ),
+      backgroundColor: Colors.grey[400],
+      appBar: AppBar(
+        title: Text('Jongwan Id Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[700],
+        elevation: 0.0,
       ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          children: [
+
+          ],
+        )
+      )
     );
   }
 }
