3152ea7f579a46d273e553df59b98ddfbbe79910
jongwankim <jongbell4@gmail.com>
2023-06-07 16:45:34 +0900

    feat: sizedBox

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,62 @@ class JWCard extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
+            Text(
+                'Hello',
+                style: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0
+                  )
+              ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+                'SHUN',
+                style: TextStyle(
+                    color: Colors.pinkAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+                'CURRENT LEVEL',
+                style: TextStyle(
+                    color: Colors.amber[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                  )
+              ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.greenAccent
+                ),
+                SizedBox(
+                  width: 10.0
+                ),
+                Text(
+                  'test@gmail.com',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.greenAccent,
+                    letterSpacing: 1.0
+                  ),
+                ),
+              ],
+            ),
           ],
         )
       )
