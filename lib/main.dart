5b46f416336f540707e89f56a40a78b78c079afd
jongwankim <jongbell4@gmail.com>
2023-06-22 17:15:07 +0900

    feat: list.map()

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-
+  home: QuoteList(),
 ));
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    '123123123',
+    '5345345345345',
+    '567568678769789789'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) {
+          return Text(quote);
+        }).toList(),
+      ),
+    );
+  }
+}
