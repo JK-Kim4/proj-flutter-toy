d9e26978f8e450362dc136a34427886ec49bf340
jongwankim <jongbell4@gmail.com>
2023-06-27 17:35:23 +0900

    feat: extract .dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList(),
@@ -31,41 +32,6 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
-      ),
-    );
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-
-  final Quote quote;
-  QuoteCard({ required this.quote });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600]
-              ),
-            ),
-            SizedBox(height: 6.0,),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800]
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
---
+++ lib/quote_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+
+  final Quote quote;
+  QuoteCard({ required this.quote });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600]
+              ),
+            ),
+            SizedBox(height: 6.0,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800]
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
