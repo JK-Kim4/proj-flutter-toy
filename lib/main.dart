94fe9c360684c3f4eac1a576138a39fb20e94d91
jongwankim <jongbell4@gmail.com>
2023-07-07 16:08:19 +0900

    feat: rename project / new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,13 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'package:flutter_toy/pages/choose_location.dart';
+import 'package:flutter_toy/pages/loading.dart';
+import 'package:flutter_toy/pages/home.dart';
 
 void main() => runApp(MaterialApp(
-  home: QuoteList(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
 ));
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Quote> quotes = [
-    Quote(author: 'Oscar Wilde', text: 'test text here 1'),
-    Quote(author: 'Oscar Wilde', text: 'test text here 2'),
-    Quote(author: 'Oscar Wilde', text: 'test text here 3'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-          quote: quote,
-          delete: (){
-            setState(() {
-              quotes.remove(quote);
-            });
-          }
-        )).toList(),
-      ),
-    );
-  }
-}
