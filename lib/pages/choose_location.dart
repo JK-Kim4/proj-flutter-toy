aba10af55c68204c28d140511496c4ee0845946c
jongwankim <jongbell4@gmail.com>
2023-07-07 16:17:03 +0900

    feat:  routing pages

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,6 +11,13 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('choose location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
       body: Text('Choose location screen'),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,20 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('home screen')),
+      body: SafeArea(
+          child: Column(
+            children: [
+              TextButton.icon(
+                  onPressed: (){
+                    Navigator.pushNamed(
+                        context,
+                        '/location'
+                    );
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text('edit button'))
+            ],
+          )),
     );
   }
 }
