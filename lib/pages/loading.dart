6e1ace5add431e82b2db751b712e9705a1870544
jongwan <jongbell4@gmail.com>
2023-07-13 22:03:03 +0900

    feat: api data

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
+import 'package:flutter_toy/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -13,27 +10,31 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  String time = 'loading';
 
-    final uri = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
-    final response = await http.get(uri);
-
-    Map data = jsonDecode(response.body);
-
-    print(data['userId']);
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'seoul', flag: 'korea.png', url: 'asia/seoul');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
+
 
   @override
   void initState() {
     super.initState();
-    getData();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading screen'),
+      body:Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(time)
+      ),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class WorldTime{
+
+  late String location; // location name
+  late String time; // the time in this location
+  late String flag; // url to assets flag icon
+  late String url; //url endpoint
+
+  WorldTime({ required this.location, required this.flag, required this.url });
+
+  Future<void> getTime() async {
+    final uri = Uri.parse('https://worldtimeapi.org/api/timezone/$url');
+    final response = await http.get(uri);
+
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'];
+    //print(datetime);
+    //print(offset);
+    offset = offset.substring(1,3);
+
+
+    //create datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set time property
+    time = now.toString();
+  }
+
+}
